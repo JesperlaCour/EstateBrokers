0bfbbcd15183687d709d9ead5c6a052e6e44e6e1
Jesper la Cour <Jesper_laCour@me.com>
2020-11-16 11:42:29 +0100

    SolutionAdded
    Solution oprettet

---
+++ EstateBrokers/Controller/ControllerSingleton.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Controller
+{
+    public class ControllerSingleton
+    {
+        private static ControllerSingleton _instance;
+        CRUDLogic crud;
+
+        protected ControllerSingleton()
+        {
+            crud = new CRUDLogic();
+        }
+        public static ControllerSingleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ControllerSingleton();
+            }
+            return _instance;
+        }
+    }
+}
