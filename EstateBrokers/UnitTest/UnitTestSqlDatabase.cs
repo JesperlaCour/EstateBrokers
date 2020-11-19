63fd6f057ada9d768b0e8dbb5883773b3f128f6d
Marc3411 <64631831+Marc3411@users.noreply.github.com>
2020-11-19 15:51:18 +0100

    GetCustomer update and test

---
+++ EstateBrokers/UnitTest/UnitTestSqlDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Persistence;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestSqlDatabase
+    {
+        [TestMethod]
+        public void GetCustomerByCustomerId()
+        {
+            IDatabase testDb = new SqlDatabase();
+
+            string actual = testDb.GetCustomer(1).Name;
+            string expected = "Marc";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
