80cd808ac5186eb250fb3603a9fae625c27ed14f
Jesper la Cour <Jesper_laCour@me.com>
2020-11-20 11:19:04 +0100

    CleanedControllerPattern

---
+++ EstateBrokers/Controller/EstateControllerSingleton.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Persistence;
+
+namespace Controller
+{
+    public class EstateControllerSingleton
+    {
+        private static EstateControllerSingleton _instance;
+
+        protected EstateControllerSingleton()
+        {
+        }
+        public static EstateControllerSingleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new EstateControllerSingleton();
+            }
+            return _instance;
+        }
+        public void CreateEstate(Estate estate)
+        {
+            IDatabase db = new DatabaseProxy();
+            db.CreateEstate(estate);
+        }
+
+        IDatabase db;
+        public EstateBrokersContext GetGridEstateData()
+        {
+            db = new DatabaseProxy();
+            return db.GetGridEstateData();
+        }
+
+        public void UpdateGridEstateData()
+        {
+            db.UpdateGridEstateData();
+        }
+    }
+}
