cd292d09b09459be4396935f86eace728cdb38fc
Dasse4 <64771911+Dasse4@users.noreply.github.com>
2020-11-23 09:14:19 +0100

    Opret sag og hus gui
    gui opret hus og sag

--- EstateBrokers/GUI/OpretNytHusForms.Designer.cs
+++ EstateBrokers/GUI/OpretNytHusForms.Designer.cs
@@ -48,6 +48,7 @@ namespace GUI
             this.label10 = new System.Windows.Forms.Label();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -210,11 +211,21 @@ namespace GUI
             this.comboBox2.Size = new System.Drawing.Size(151, 28);
             this.comboBox2.TabIndex = 21;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Opret hus";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // OpretNytHusForms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(440, 415);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.label10);
@@ -263,5 +274,6 @@ namespace GUI
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
