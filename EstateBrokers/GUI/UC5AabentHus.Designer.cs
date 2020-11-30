5452e78367cd8a5d9a5e0c56a5a029ad51326533
Dasse4 <64771911+Dasse4@users.noreply.github.com>
2020-11-30 10:00:30 +0100

    Gui
    Gui til openhouse

--- EstateBrokers/GUI/UC5AabentHus.Designer.cs
+++ EstateBrokers/GUI/UC5AabentHus.Designer.cs
@@ -31,12 +31,23 @@ namespace GUI
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btn_ShowExisting = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.btn_ChooseHouse = new System.Windows.Forms.Button();
+            this.btn_RemoveHouse = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_Assigned = new System.Windows.Forms.Button();
+            this.lbl_Broker1 = new System.Windows.Forms.Label();
+            this.lbl_Broker2 = new System.Windows.Forms.Label();
+            this.lbl_Broker3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -50,77 +61,190 @@ namespace GUI
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(203, 90);
+            this.comboBox1.Location = new System.Drawing.Point(1482, 150);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(151, 28);
             this.comboBox1.TabIndex = 2;
             // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(360, 90);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(151, 28);
-            this.comboBox2.TabIndex = 3;
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(517, 90);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(151, 28);
-            this.comboBox3.TabIndex = 4;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 67);
+            this.label1.Font = new System.Drawing.Font("Courier New", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(925, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 20);
+            this.label1.Size = new System.Drawing.Size(339, 34);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "Arrengere åbenthus";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(360, 67);
+            this.label2.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(249, 94);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(80, 17);
             this.label2.TabIndex = 6;
-            this.label2.Text = "label2";
+            this.label2.Text = "Adresse:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(517, 67);
+            this.label3.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(1131, 94);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.Size = new System.Drawing.Size(166, 27);
             this.label3.TabIndex = 7;
-            this.label3.Text = "label3";
+            this.label3.Text = "Valgte huse";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(203, 150);
+            this.listBox1.Location = new System.Drawing.Point(249, 150);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(450, 364);
+            this.listBox1.Size = new System.Drawing.Size(450, 904);
             this.listBox1.TabIndex = 8;
             // 
-            // AabentHus
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(984, 150);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(450, 904);
+            this.listBox2.TabIndex = 9;
+            // 
+            // btn_ShowExisting
+            // 
+            this.btn_ShowExisting.Location = new System.Drawing.Point(245, 62);
+            this.btn_ShowExisting.Name = "btn_ShowExisting";
+            this.btn_ShowExisting.Size = new System.Drawing.Size(129, 29);
+            this.btn_ShowExisting.TabIndex = 10;
+            this.btn_ShowExisting.Text = "Se eksisterende";
+            this.btn_ShowExisting.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(249, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 11;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(380, 117);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(94, 29);
+            this.btn_Search.TabIndex = 12;
+            this.btn_Search.Text = "Søg";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            // 
+            // btn_ChooseHouse
+            // 
+            this.btn_ChooseHouse.Location = new System.Drawing.Point(792, 404);
+            this.btn_ChooseHouse.Name = "btn_ChooseHouse";
+            this.btn_ChooseHouse.Size = new System.Drawing.Size(94, 29);
+            this.btn_ChooseHouse.TabIndex = 13;
+            this.btn_ChooseHouse.Text = "button3";
+            this.btn_ChooseHouse.UseVisualStyleBackColor = true;
+            // 
+            // btn_RemoveHouse
+            // 
+            this.btn_RemoveHouse.Location = new System.Drawing.Point(792, 463);
+            this.btn_RemoveHouse.Name = "btn_RemoveHouse";
+            this.btn_RemoveHouse.Size = new System.Drawing.Size(94, 29);
+            this.btn_RemoveHouse.TabIndex = 14;
+            this.btn_RemoveHouse.Text = "button4";
+            this.btn_RemoveHouse.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(1482, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Mæglere";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(1482, 349);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(1482, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Åbenhus dato:";
+            // 
+            // btn_Assigned
+            // 
+            this.btn_Assigned.Location = new System.Drawing.Point(1482, 401);
+            this.btn_Assigned.Name = "btn_Assigned";
+            this.btn_Assigned.Size = new System.Drawing.Size(94, 29);
+            this.btn_Assigned.TabIndex = 18;
+            this.btn_Assigned.Text = "Tildel";
+            this.btn_Assigned.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Broker1
+            // 
+            this.lbl_Broker1.AutoSize = true;
+            this.lbl_Broker1.Location = new System.Drawing.Point(1482, 197);
+            this.lbl_Broker1.Name = "lbl_Broker1";
+            this.lbl_Broker1.Size = new System.Drawing.Size(50, 20);
+            this.lbl_Broker1.TabIndex = 19;
+            this.lbl_Broker1.Text = "label6";
+            // 
+            // lbl_Broker2
+            // 
+            this.lbl_Broker2.AutoSize = true;
+            this.lbl_Broker2.Location = new System.Drawing.Point(1482, 237);
+            this.lbl_Broker2.Name = "lbl_Broker2";
+            this.lbl_Broker2.Size = new System.Drawing.Size(50, 20);
+            this.lbl_Broker2.TabIndex = 20;
+            this.lbl_Broker2.Text = "label7";
+            // 
+            // lbl_Broker3
+            // 
+            this.lbl_Broker3.AutoSize = true;
+            this.lbl_Broker3.Location = new System.Drawing.Point(1482, 274);
+            this.lbl_Broker3.Name = "lbl_Broker3";
+            this.lbl_Broker3.Size = new System.Drawing.Size(50, 20);
+            this.lbl_Broker3.TabIndex = 21;
+            this.lbl_Broker3.Text = "label8";
+            // 
+            // UC5AabentHus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lbl_Broker3);
+            this.Controls.Add(this.lbl_Broker2);
+            this.Controls.Add(this.lbl_Broker1);
+            this.Controls.Add(this.btn_Assigned);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_RemoveHouse);
+            this.Controls.Add(this.btn_ChooseHouse);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_ShowExisting);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.panel1);
             this.MinimumSize = new System.Drawing.Size(1920, 1080);
-            this.Name = "AabentHus";
+            this.Name = "UC5AabentHus";
             this.Size = new System.Drawing.Size(1920, 1080);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -131,11 +255,22 @@ namespace GUI
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btn_ShowExisting;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Button btn_ChooseHouse;
+        private System.Windows.Forms.Button btn_RemoveHouse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_Assigned;
+        private System.Windows.Forms.Label lbl_Broker1;
+        private System.Windows.Forms.Label lbl_Broker2;
+        private System.Windows.Forms.Label lbl_Broker3;
     }
 }
